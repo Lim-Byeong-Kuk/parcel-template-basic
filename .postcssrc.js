4825796e114220135c19d51ac6fc0d52715d1501
Lim-Byeong-Kuk <bye0414@naver.com>
2023-07-14 17:39:29 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,14 @@
+
+// import autoprfixer from 'autoprefixer'
+const autoprefixer = require('autoprefixer');
+
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+};
